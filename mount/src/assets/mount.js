2e35c68c9b5cb1009409e134650b9dd00a47437f
achal6076 <Achal6076@gmail.com>
2023-02-27 14:40:20 +0530

    initial

---
+++ mount/src/assets/mount.js
@@ -0,0 +1,33 @@
+
+import React from "react";
+import axios from "axios";
+import Posts from "./post";
+import Comments from "./comment";
+import Users from "./user";
+
+export default class ComponentClass extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      hello: true,
+      renderType: "posts",
+      items: [],
+    };
+  }
+  render() {
+    return (
+      <div class = "card">
+        <br />
+        <center>
+          <button onClick={() => this.setState({ renderType: "posts" })}>Posts</button>
+          <button onClick={() => this.setState({ renderType: "comments" })}>Comments</button>
+          <button onClick={() => this.setState({ renderType: "users" })}>Users</button>
+        </center>
+        {this.state.renderType==="users" && <Users users ={this.state.items}/>}
+        {this.state.renderType==="posts" && <Posts posts ={this.state.items}/>}
+        {this.state.renderType==="comments" && <Comments comments ={this.state.items}/>}
+        {/* {this.state.renderType=== "posts" ? <Posts/> : (this.state.renderType === "comments" ? <Comments/> : <Users/>)}  */}
+      </div>
+    );
+  }
+}
---
+++ mount/src/assets/post.js
@@ -0,0 +1,52 @@
+import React from "react";
+import axios from "axios";
+
+export default class Posts extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderType: "posts",
+      items: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMountCalled", this.state.renderType);
+    axios
+      .get(`https://jsonplaceholder.typicode.com/${this.state.renderType}`)
+      .then((res) =>
+        this.setState({
+          items: res.data,
+        })
+      );
+  }
+
+
+
+  render() {
+    return (
+      <div>
+        <table border={3}>
+          <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Body</th>
+          </tr>
+
+          <tbody>
+            {this.state.items.map((item) => {
+              return (
+                <tr>
+                  {/* <td>{item.userId}</td> */}
+                  <td>{item.id}</td>
+                  <td>{item.title}</td>
+                  <td>{item.body}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
