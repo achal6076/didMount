e60fea311b92690790ccadd822a00186e4aac5b9
achal6076 <Achal6076@gmail.com>
2023-02-28 18:42:37 +0530

    component remove

--- mount/src/index.js
+++ mount/src/index.js
@@ -1,16 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
 // import App from './App';
-import reportWebVitals from './reportWebVitals';
-import ComponentClass from './assets/mount.js'
+import reportWebVitals from "./reportWebVitals";
+import ComponentClass from "./assets/mount.js";
+import { BrowserRouter} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
+
+  
+  // <React.StrictMode>
+  //   {/* <App /> */}
+  //   <ComponentClass/>
+  // </React.StrictMode>
+
+  <BrowserRouter>
+    {/* <Routes>
+      <Route path="/" element={ComponentClass} />
+    </Routes> */}
     <ComponentClass/>
-  </React.StrictMode>
+
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
